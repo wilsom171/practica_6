3612f4cee534ba47734c2a3e32872dfb845e4488
wilsom171 <53121506+wilsom171@users.noreply.github.com>
2021-08-08 15:52:56 -0500

    Update app.js

--- src/app.js
+++ src/app.js
@@ -10,7 +10,7 @@ const app = express();
 const customerRoutes = require('./routes/customer');
 
 // settings
-app.set('port', process.env.PORT || 3000);
+app.set('port', process.env.PORT || 5000);
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
